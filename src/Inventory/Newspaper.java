20a0d95aa0557546e0ba64623056b6a2d13ef784
izen57 <golovoslon@gmail.com>
2023-12-05 16:28:16 +0100

    feat: create a newspaper item

---
+++ src/Inventory/Newspaper.java
@@ -0,0 +1,16 @@
+package Inventory;
+
+public class Newspaper extends NonApplicableItem
+{
+    public Newspaper()
+    {
+        super("The forgotten newspaper in the French Painting Collections",
+            "You picked up a newspaper from a week ago. The front page reads, " +
+                "\"The Louvre will redistribute some of its exhibits. " +
+                "Due to the expected influx of visitors and the poor condition of some storage areas, " +
+                "the main museum of France and Paris plans to send some exhibits, mainly French painters and sculptors, for restoration. " +
+                "Museum officials also said that to ensure proper conditions for the cats that live in the museum buildings and that are so beloved by the public, " +
+                "the staff is willing to temporarily take the four-legged creatures back to their homes.\""
+        );
+    }
+}
