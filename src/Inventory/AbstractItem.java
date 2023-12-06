311fe82c38e1c6a9c9d5e0ea5b6638cff47d3308
izen57 <golovoslon@gmail.com>
2023-12-06 14:26:17 +0100

    feat: delete setters for AbstractItem

--- src/Inventory/AbstractItem.java
+++ src/Inventory/AbstractItem.java
@@ -25,15 +25,7 @@ public abstract class AbstractItem
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
