03e6fef7a3477b31816380842c5ae6e201347d25
izen57 <golovoslon@gmail.com>
2023-12-06 16:15:01 +0100

    fix: documentation for the Inventory with the main package

--- src/Inventory/Inventory.java
+++ src/Inventory/Inventory.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * A class which represents an inventory of an in-game instance.
- * The inventory is held by {@link Locations.Location locations} and all classes derived by the {@link Character character} class.
+ * The inventory is held by {@link Locations.Location locations} and all classes derived by the {@link Main.Character character} class.
  */
 public class Inventory
 {
