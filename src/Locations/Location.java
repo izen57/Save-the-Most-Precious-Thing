13570becb440edf060c39559923eef647e2c43fc
izen57 <golovoslon@gmail.com>
2023-12-06 16:29:50 +0100

    fix: delete setters for the Location

--- src/Locations/Location.java
+++ src/Locations/Location.java
@@ -39,16 +39,8 @@ public class Location {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     /**
