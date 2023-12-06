770d51fc24d2801d900e7160065b854315c5af9d
izen57 <golovoslon@gmail.com>
2023-12-06 14:30:25 +0100

    feat: write documentation for ApplicableItem

--- src/Inventory/ApplicableItem.java
+++ src/Inventory/ApplicableItem.java
@@ -1,7 +1,8 @@
 package Inventory;
 
 /**
- * An applicable item can be applied in some situation to another applicable item only.
+ * An applicable item can be applied in some situation.
+ * Currently, there is no crucial differences between an applicable item and non-applicable one.
  */
 public abstract class ApplicableItem extends AbstractItem implements IApplicable
 {
