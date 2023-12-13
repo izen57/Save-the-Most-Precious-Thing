85af4ad0d3271ba998e217ba18f8243c12448780
izen57 <golovoslon@gmail.com>
2023-12-13 18:38:31 +0100

    fix: ItalianPaintingCollections constructor

--- src/Locations/ItalianPaintingCollections.java
+++ src/Locations/ItalianPaintingCollections.java
@@ -1,8 +1,15 @@
 package Locations;
 
+import Inventory.Inventory;
+
 public class ItalianPaintingCollections extends Location {
     public ItalianPaintingCollections() {
-        super("italianPaintingCollections", null, "It is filled with paintings by Italian painters, Titian, Leonardo da Vinci, Caravaggio, but the important thing is that you need to find the location of the Mona Lisa.");
-//        super.inventory.addItem(new Key());
+        super(
+            "italianPaintingCollections",
+            new Inventory(),
+            "It is filled with paintings by Italian painters, Titian, " +
+                "Leonardo da Vinci, Caravaggio, but the important thing is " +
+                "that you need to find the location of the Mona Lisa."
+        );
   }
 }
