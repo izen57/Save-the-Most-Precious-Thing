050d51e1c77e2767b39c1efdebe4e98ec5f7ce02
izen57 <golovoslon@gmail.com>
2023-12-13 18:26:05 +0100

    fix: AncientGreekGallery message

--- src/Locations/AncientGreekGallery.java
+++ src/Locations/AncientGreekGallery.java
@@ -9,8 +9,8 @@ public class AncientGreekGallery extends Location {
             "ancientGreekGallery",
             new Inventory(),
             "Now you have entered the Denon Hall and walked straight into the corridor filled with ancient Greek sculptures. " +
-                "On your right is the statue of Venus with a broken arm. She is at the end of the corridor,facing the corridor and " +
-                "quietly overlooking the countless worldly people passing by."
+                "On your right is the statue of Venus with a broken arm. She stands at the end of the corridor, facing the corridor and " +
+                "silently watching the countless worldly people passing by."
         );
     }
 
