6dc8d68a4b69857848dec34183420ba02bf9a16c
izen57 <golovoslon@gmail.com>
2024-01-02 14:54:28 +0100

    fix message

--- src/Locations/AncientGreekGallery.java
+++ src/Locations/AncientGreekGallery.java
@@ -9,7 +9,7 @@ public class AncientGreekGallery extends Location {
             "ancientGreekGallery",
             new Inventory(),
             "Now you have entered the Denon Hall and walked straight into the corridor filled with ancient Greek sculptures. " +
-                "to your south is the statue of Venus with a broken arm. ",
+                "To your south is the statue of Venus with a broken arm.",
             "Venus is at the end of the corridor, facing the long corridor and " +
                 "quietly overlooking the countless worldly people passing by."
         );
