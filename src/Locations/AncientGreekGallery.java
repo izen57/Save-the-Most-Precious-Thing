000ef939b1caae9b09e67d52d2af5d28c44f8862
Pikajojo <wangyajing1016@gmail.com>
2024-01-05 11:54:51 +0100

    space

--- src/Locations/AncientGreekGallery.java
+++ src/Locations/AncientGreekGallery.java
@@ -16,9 +16,9 @@ public class AncientGreekGallery extends Location {
             "ancientGreekGallery",
             new Inventory(),
             "Now you have entered the Denon Hall and walked straight into the corridor filled with ancient Greek sculptures. " +
-                "To your south is the statue of Venus with a broken arm.",
+                     "To your south is the statue of Venus with a broken arm.",
             "Venus is at the end of the corridor, facing the long corridor and " +
-                "quietly overlooking the countless worldly people passing by."
+                      "quietly overlooking the countless worldly people passing by."
         );
     }
 }
--- src/Locations/ApolloGallery.java
+++ src/Locations/ApolloGallery.java
@@ -17,10 +17,10 @@ public class ApolloGallery extends Location {
             "apolloGallery",
             new Inventory(),
             "You have entered the Apollo Gallery, filled with dazzling jewelry and royal paraphernalia. " +
-                "The king's crown stands in the center. " +
-                "Signs to help visitors navigate their way point both north and east.",
+                     "The king's crown stands in the center. " +
+                     "Signs to help visitors navigate their way point both north and east.",
             "There is a Monet painting hanging at the north exit, " +
-                "and the lights at the east exit are very bright."
+                      "and the lights at the east exit are very bright."
         );
     }
 }
